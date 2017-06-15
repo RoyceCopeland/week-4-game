ec068956cabcce7f79f127ad937c15f9859780e2
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-06-15 15:28:22 -0400

    working checkpoint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,79 @@
 "use strict";
 $(document).ready(function() {
-        var randomTargetNumber;
-        var randomNumberSkull1;
-        var randomNumberSkull2;
-        var randomNumberSkull3;
-        var randomNumberSkull4;
+    var randomTargetNumber;
+    var randomNumberSkull1;
+    var randomNumberSkull2;
+    var randomNumberSkull3;
+    var randomNumberSkull4;
+    var totalScore = 0;
 
 
-        //There will be four crystals displayed as buttons on the page.
+    //There will be four crystals displayed as buttons on the page.
 
 
-        //The player will be shown a random number at the start of the game.
-        $("#randomNumberButton").on("click", function() {
-            var randomTargetNumber = Math.floor(Math.random() * (102) + 19);
-            console.log(randomTargetNumber);
+    //The player will be shown a random number at the start of the game.
+    $("#randomNumberButton").on("click", function() {
+        var randomTargetNumber = Math.floor(Math.random() * (102) + 19);
+        randomNumberSkull1 = Math.floor(Math.random() * (11) + 1);
+        randomNumberSkull2 = Math.floor(Math.random() * (11) + 1);
+        randomNumberSkull3 = Math.floor(Math.random() * (11) + 1);
+        randomNumberSkull4 = Math.floor(Math.random() * (11) + 1);
 
-            $("#targetScore").html(randomTargetNumber);
+        // console.log(randomTargetNumber);
+        // console.log(randomNumberSkull1);
+        // console.log(randomNumberSkull2);
+        // console.log(randomNumberSkull3);
+        // console.log(randomNumberSkull4);
 
-        })
+        $("#targetScore").html(randomTargetNumber);
+
+
 
 
         //When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+        //Your game will hide this amount until the player clicks a crystal.
+
         // That amount of points must stay the same throught a given game.
 
+        //When they do click one, update the player's score counter.
+
+
         $("#sugarSkull1").on("click", function() {
-            var randomNumberSkull1 = Math.floor(Math.random() * (11) + 1);
+            totalScore = totalScore + randomNumberSkull1;
+            $("#playerScore").html(totalScore);
             console.log(randomNumberSkull1);
-
-
-
-
+            console.log(totalScore);
         })
 
         $("#sugarSkull2").on("click", function() {
-            var randomNumberSkull2 = Math.floor(Math.random() * (11) + 1);
+            totalScore = totalScore + randomNumberSkull2;
+            $("#playerScore").html(totalScore);
             console.log(randomNumberSkull2);
 
-
+            console.log(totalScore);
         })
 
         $("#sugarSkull3").on("click", function() {
-            var randomNumberSkull3 = Math.floor(Math.random() * (11) + 1);
+            totalScore = totalScore + randomNumberSkull3;
+            $("#playerScore").html(totalScore);
             console.log(randomNumberSkull3);
 
-
+            console.log(totalScore);
         })
 
         $("#sugarSkull4").on("click", function() {
-                var randomNumberSkull4 = Math.floor(Math.random() * (11) + 1);
-                console.log(randomNumberSkull4);
+            totalScore = totalScore + randomNumberSkull4;
+            $("#playerScore").html(totalScore);
+            console.log(randomNumberSkull4);
 
+            console.log(totalScore);
 
-            })
-            //Your game will hide this amount until the player clicks a crystal.
+        })
 
 
     })
-    //When they do click one, update the player's score counter.
+
+})
 
 
 
