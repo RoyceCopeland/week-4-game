28b0774609b56c66ea1b1fc1abedc3756ba925e4
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-06-15 10:17:49 -0400

    working checkpoint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,22 @@
+"use strict";
+$(document).ready(function() {
+    var randomNumber;
 
 
-//There will be four crystals displayed as buttons on the page.
+    //There will be four crystals displayed as buttons on the page.
 
 
-//The player will be shown a random number at the start of the game.
+    //The player will be shown a random number at the start of the game.
+    $("#randomNumberButton").on("click", function() {
+        var randomNumber = Math.floor(Math.random() * (102) + 19);
+        console.log(randomNumber);
 
+           $("#targetScore").html(randomNumber);
+             //      document.getElementById("#guessPanel").innerHTML = answerArray;
+         
 
-
+    })
+})
 
 //When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 
